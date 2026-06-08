643744d8d9270cd02ff272ffa0fca8fa5e9cd13a
proddy <paul@derbyshire.nl>
2026-06-08 20:41:31 +0200

    formatting

--- mock-api/restServer.ts
+++ mock-api/restServer.ts
@@ -318,10 +318,10 @@ function updateMask(entity: any, de: any, dd: any) {
       const old_custom_name = dd.nodes[dd_objIndex].cn;
       console.log(
         'comparing names, old (' +
-        old_custom_name +
-        ') with new (' +
-        new_custom_name +
-        ')'
+          old_custom_name +
+          ') with new (' +
+          new_custom_name +
+          ')'
       );
       if (old_custom_name !== new_custom_name) {
         changed = true;
@@ -438,9 +438,9 @@ function upgradeImportantMessages(version: string) {
 
   console.log(
     'upgradeImportantMessageType: version=' +
-    version +
-    ' type=' +
-    upgradeImportantMessageType_n
+      version +
+      ' type=' +
+      upgradeImportantMessageType_n
   );
   return { upgradeImportantMessageType: upgradeImportantMessageType_n };
 }
@@ -487,17 +487,17 @@ function get_versions() {
 
   console.log(
     'getVersions: current=' +
-    THIS_VERSION +
-    ' stable=' +
-    LATEST_STABLE_VERSION +
-    ' (upgradeable=' +
-    (STABLE_VERSION_IS_UPGRADEABLE ? 'YES' : 'NO') +
-    ') dev=' +
-    LATEST_DEV_VERSION +
-    ' (upgradeable=' +
-    (DEV_VERSION_IS_UPGRADEABLE ? 'YES' : 'NO') +
-    ')' +
-    (MOCK_OFFLINE ? ' [offline]' : '')
+      THIS_VERSION +
+      ' stable=' +
+      LATEST_STABLE_VERSION +
+      ' (upgradeable=' +
+      (STABLE_VERSION_IS_UPGRADEABLE ? 'YES' : 'NO') +
+      ') dev=' +
+      LATEST_DEV_VERSION +
+      ' (upgradeable=' +
+      (DEV_VERSION_IS_UPGRADEABLE ? 'YES' : 'NO') +
+      ')' +
+      (MOCK_OFFLINE ? ' [offline]' : '')
   );
   return data;
 }
@@ -4796,7 +4796,9 @@ router
       }
 
       // add the scheduler data
-      const namedSchedules = emsesp_schedule.schedule.filter((item: any) => item.name);
+      const namedSchedules = emsesp_schedule.schedule.filter(
+        (item: any) => item.name
+      );
       if (namedSchedules.length > 0) {
         const scheduler_data = namedSchedules.map((item: any, index: number) => ({
           id: DeviceTypeUniqueID.SCHEDULER_UID * 100 + index,
@@ -4816,7 +4818,9 @@ router
       }
 
       // add the command items (executable from dashboard)
-      const namedCommands = emsesp_commands.commands.filter((item: any) => item.name);
+      const namedCommands = emsesp_commands.commands.filter(
+        (item: any) => item.name
+      );
       if (namedCommands.length > 0) {
         const command_data = namedCommands.map((item: any, index: number) => ({
           id: DeviceTypeUniqueID.COMMAND_UID * 100 + index,
