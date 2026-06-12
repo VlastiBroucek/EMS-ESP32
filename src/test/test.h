e3bec544beca6e8601c26f368ec7f6adf3865b4d
proddy <paul@derbyshire.nl>
2026-06-12 16:29:26 +0200

    version test

--- src/test/test.h
+++ src/test/test.h
@@ -65,7 +65,7 @@ namespace emsesp {
 // #define EMSESP_DEBUG_DEFAULT "shuntingyard"
 // #define EMSESP_DEBUG_DEFAULT "src"
 // #define EMSESP_DEBUG_DEFAULT "led"
-#define EMSESP_DEBUG_DEFAULT "version"
+// #define EMSESP_DEBUG_DEFAULT "version"
 
 #ifndef EMSESP_DEBUG_DEFAULT
 #define EMSESP_DEBUG_DEFAULT "general"
