a20d1d8970fdba1299eb33637976aca0ab0bedbb
MichaelDvP <github@mdvp.de>
2026-07-12 10:17:02 +0200

    fix typo

--- src/core/system.cpp
+++ src/core/system.cpp
@@ -1952,7 +1952,7 @@ bool System::get_value_info(JsonObject output, const char * cmd) {
         LOG_ERROR("empty system command");
         return false;
     }
-    if (!strcmp(cmd, "reset")) { // reset is a command, not an entity
+    if (!strcmp(cmd, "restart")) { // restart is a command, not an entity
         return false;
     }
     // check for hardcoded "info"/"value"
