b1ebd81ca2ddc552e867d4196717048a9bdea25c
proddy <paul@derbyshire.nl>
2026-08-01 16:43:55 +0200

    update

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -3,28 +3,34 @@
 # run from root folder, like `sh ./scripts/update_all.sh`
 # as well as GNUMake (make) and python3
 
+# abort on first error so a failed lint/build isn't silently swallowed
+set -e
+
 cd interface
 rm -rf node_modules
-corepack use pnpm@latest-10
-pnpm update --latest
+corepack use pnpm@latest
+pnpm update
 pnpm install
 pnpm format
 pnpm lint
+pnpm build-webUI
 
 cd ../mock-api
 rm -rf node_modules
-corepack use pnpm@latest-10
-pnpm update --latest
+corepack use pnpm@latest
+pnpm update
 pnpm install
 pnpm format
-
-cd ..
-cd interface
-pnpm build-webUI
 
 cd ..
 npx cspell "**"
 
-# platformio run -e build_modbus
-# platformio run -e build_standalone
-# platformio run -e native-test -t exec
+# build files that go into docs folder
+
+platformio run -e build_modbus -t clean
+platformio run -e build_modbus
+platformio run -e build_standalone -t clean
+platformio run -e build_standalone
+
+# run tests
+platformio run -e native-test -t exec
