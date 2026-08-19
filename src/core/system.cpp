edd167752643f730ed1a132254c549eba8133d85
MichaelDvP <github@mdvp.de>
2026-08-19 11:49:04 +0200

    http timeouts

--- src/core/system.cpp
+++ src/core/system.cpp
@@ -160,7 +160,9 @@ bool System::command_sendmail(const char * value, const int8_t) {
 
     ssl_client->setClient(basic_client);
     ssl_client->setInsecure();
-    ssl_client->setBufferSizes(1024, 1024);
+    ssl_client->setBufferSizes(16384, 1024);
+    basic_client->setTimeout(5000); // socket-level read timeout
+    ssl_client->setTimeout(5000);   // Stream::readBytes timeout used by Update
     r_client->addPort(port,
                       security == EMAIL_SECURITY::NONE  ? readymail_protocol_plain_text
                       : security == EMAIL_SECURITY::SSL ? readymail_protocol_ssl
